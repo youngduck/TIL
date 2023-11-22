75c27c861831c0dc625331675ead644ce635d967
youngduck <dudejr5839@naver.com>
2023-11-22 15:31:40 +0900

    Reat-Query: useQuery 훅 & 중앙집중화

---
+++ React-Query/REACT-QUERY-GUIDE/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query';
+
+import type { Treatment } from '../../../../../shared/types';
+import { axiosInstance } from '../../../axiosInstance';
+import { queryKeys } from '../../../react-query/constants';
+// import { useCustomToast } from '../../app/hooks/useCustomToast';
+
+// for when we need a query function for useQuery
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
+
+export function useTreatments(): Treatment[] {
+  const fallback = [];
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
+  return data;
+}
