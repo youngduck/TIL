961981a57d90ed0e2a1bd0ffc0fe83f9b5aa3f06
youngduck <dudejr5839@gmail.com>
2023-12-16 23:04:13 +0900

    Yarn: Yarn-berry,TS,CRA setting

---
+++ Yarn/yarn-react-cra/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+     <div><h1>안녕</h1></div>
+    </div>
+  );
+}
+
+export default App;
