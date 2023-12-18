3802b9b9cd68234fcb15122b8a5d23bdb8ad4686
youngduck <dudejr5839@naver.com>
2023-12-18 18:15:19 +0900

    Node.js: MongoDB+express+mongoose CRUD연결

--- Node.js/Nodejs-Guide/Rest-API/app.js
+++ Node.js/Nodejs-Guide/Rest-API/app.js
@@ -1,4 +1,6 @@
+const path = require("path");
 const express = require("express");
+
 const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
 const mongoose = require("mongoose");
@@ -7,21 +9,28 @@ const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(cors());
 
 app.use((req, res, next) => {
-	next();
+  next();
 });
 
 app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
-	.connect(
-		"mongodb+srv://dudejr5839:ANfJXBfoxfeHdShe@cluster0.y5pywm5.mongodb.net/messages?retryWrites=true&w=majority"
-	)
-	.then((result) => {
-		app.listen(8080);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(
+    "mongodb+srv://dudejr5839:ANfJXBfoxfeHdShe@cluster0.y5pywm5.mongodb.net/messages?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Node.js/Nodejs-Guide/Rest-API/controllers/feed.js
+++ Node.js/Nodejs-Guide/Rest-API/controllers/feed.js
@@ -3,49 +3,68 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-	res.status(200).json({
-		posts: [
-			{
-				_id: "1",
-				title: "First Post",
-				content: "This is the first post !",
-				imgUrl: "images/car.png",
-				creator: {
-					name: "Max",
-				},
-				createdAt: new Date(),
-			},
-		],
-	});
+  Post.find()
+    .then((posts) => {
+      res
+        .status(200)
+        .json({ message: "Fetched posts succesfully.", posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
-	const errors = validationResult(req);
-	if (!errors.isEmpty()) {
-		return res.status(422).json({
-			message: "Validation failed, entered data is incoreect.",
-			errors: errors.array(),
-		});
-	}
-	const title = req.body.title;
-	const content = req.body.content;
-	const post = new Post({
-		title: title,
-		content: content,
-		imageUrl: "images/duck.jpg",
-		creator: { name: "Max" },
-	});
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed, entered data is incoreect.");
+    error.statusCode = 422;
+    throw error;
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/car.png",
+    creator: { name: "Max" },
+  });
 
-	post
-		.save()
-		.then((result) => {
-			console.log(result);
-			res.status(201).json({
-				message: "Post created successfully!",
-				post: result,
-			});
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Cloud not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
