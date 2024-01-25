e6a28e9d1d3cc10c4e817a416943d412920f43ce
youngduck <dudejr5839@gmail.com>
2024-01-25 20:33:29 +0900

    Algorithm: string indexing

---
+++ Algorithm/Basic/String/indexOf.js
@@ -0,0 +1,7 @@
+//한글자 위치찾기
+const word = 'abcdefg'
+console.log(word.indexOf('b'))
+
+//여러글자 위치찾기
+const worods='asdvasdf'
+console.log(words.indexOf('dv'))
---
+++ Algorithm/Basic/String/lastIndexOf.js
@@ -0,0 +1,6 @@
+//한글자 indexing
+const word = 'vasdfsavasdf'
+console.log(word.lastIndexOf('s'))
+//여러글자 indexing
+const words = 'vasdfsavasdf'
+console.log(words.lastIndexOf('va'))
