fcb2e6147e1d8ba7d5399cb6c58e88c6f22a507b
youngduck <dudejr5839@naver.com>
2024-02-29 17:51:49 +0900

    String: 모든진수변환

---
+++ Algorithm/Basic/String/jinsu.js
@@ -0,0 +1,20 @@
+let decimal = 2022;
+//10진수 -> 2진수
+console.log(decimal.toString(2));
+//10진수 -> 3진수
+console.log(decimal.toString(3));
+//10진수 -> N진수
+console.log(decimal.toString(N));
+
+//2진수 -> 10진수
+let binary = "1011"
+console.log(parseInt(number,2));
+//3진수 -> 10진수
+let trinary = "2122"
+console.log(parseInt(number,3));
+//N진수 -> 10진수
+let parameter = "~"
+parseInt(parameter,N);
+
+//N진수 -> N진수
+console.log(parseInt("21121",3).toString(2));
